1cb97180fcf4cfb9811ce938fe3ad8059ebbc15b
Borisov-Ilya <borisov.ilya22@gmail.com>
2022-01-30 23:00:16 +0300

    add 81-95 tasks BasicJS

---
+++ freecodecamp/Basic JavaScript/61-90/84.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Teo",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Cats", "Water"]
+    // Only change code above this line
+};
